1e97203de7ccd4d709a01d08bd66577f648a1d7d
HB <htb320@lehigh.edu>
2019-04-26 15:03:39 +0700

    chap3
    Chapter 4
    
    Chap 5
    
    chap6
    
    Chap 7
    
    Chap 8
    
    Chap 9
    
    Chap 9
    
    Chap9
    
    Rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: {maximum: Settings.user.name}
+	attr_accessor :remember_token
+	before_save { self.email = email.downcase }
+	validates :name, presence: true, length: { maximum: Settings.user.name }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 },
+		format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: Settings.user.email},
@@ -20,6 +25,24 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
 
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+	def forget
+		update_attribute(:remember_digest, nil)
+  end
+  
   private
   def before_save
     self.email.downcase!
