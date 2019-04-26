a4371b9642ac78c3fd8fdf6cf927c907c61f2d0d
HB <htb320@lehigh.edu>
2019-04-26 15:03:39 +0700

    Chap 10 in progress ful Pull Request

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,23 +3,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def current_user
-    @current_user || User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    current_user.present?
-  end
-
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
-
   def remember user
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def current_user? user
+    user == current_user
   end
 
   def current_user
@@ -27,16 +18,15 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user&.authenicated?(cookies[:remember_token])
+      if user && user.authenicated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
     end
   end
 
-  # Return true if the user is logged in, false otherwise
   def logged_in?
-    !current_user.nil?
+    current_user.present?
   end
 
   def forget user
@@ -45,9 +35,18 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
 
-    def log_out
-        forget(current_user)
-        session.delete(:user_id)
-        @current_user = nil
-    end
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def redirect_back_or default
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,14 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
-	before_save { self.email = email.downcase }
-	validates :name, presence: true, length: { maximum: Settings.user.name }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, length: { maximum: 255 },
-		format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
+  attr_accessor :remember_token
+  validates :name, presence: true, length: {maximum: Settings.user.name}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: Settings.user.email},
-                    format: {with: VALID_EMAIL_REGEX},
-                    uniqueness: {case_sensitive: false}
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 
   has_secure_password
-  validates :password, presence: true,
-  length: {minimum: Settings.user.password_min}
+  validates :password, presence: true, length: {minimum: Settings.user.password},
+  allow_nil: true
 
   def self.digest string
     if cost == ActiveModel::SecurePassword.min_cost
@@ -39,12 +34,12 @@ class User < ApplicationRecord
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
-	def forget
-		update_attribute(:remember_digest, nil)
+  def forget
+    update_attribute(:remember_digest, nil)
   end
-  
+
   private
   def before_save
-    self.email.downcase!
+    email.downcase!
   end
 end
