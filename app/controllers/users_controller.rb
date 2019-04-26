82e495cf215e485b77a013603e70ed1737467d44
HB <htb320@lehigh.edu>
2019-04-26 15:03:39 +0700

    chap3
    Chap 5
    
    chap6
    
    Chap 7
    
    Chap 8
    
    Fixed Rubocop
    
    Chap 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,32 @@
 class UsersController < ApplicationController
-  def new; end
+  def show
+    @user = User.find_by(params[:id])
+
+    if(!@user.present?) 
+      flash[:danger] = t("texts.invalid_user")
+      render :new
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      log_in @user
+      flash[:success] = t("texts.welcome")
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require :user.permit(:name,
+      :email, :password, :password_confirmation)
+  end
 end
