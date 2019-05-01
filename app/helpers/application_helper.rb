304bbd9f12160a2d5c7cf934021411667b6db6ef
HB <htb320@lehigh.edu>
2019-05-01 20:54:28 +0700

    Chapter 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,20 @@
 module ApplicationHelper
+<<<<<<< HEAD
   def full_title page_title = ""
     base_title = t("titleStatic")
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
 end
+=======
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = "")
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+end
+>>>>>>> Chapter 4
